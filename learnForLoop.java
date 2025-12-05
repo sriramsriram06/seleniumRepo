d495fe20c92910da4d5e307ee0c4014ce71d97f3
sriramsriram06 <ppriy37@gmail.com>
2025-12-05 11:23:12 +0530

    Add files via upload
    learn forloop

---
+++ learnForLoop.java
@@ -0,0 +1,18 @@
+package LearnAutomation1;
+
+public class learnForLoop {
+	
+	 public static void main(String[] args) {
+
+	        int num = 10;  // declare a number to add with i
+
+	        for (int i = 5; i < 10; i++) {
+
+	            System.out.println(num + " / " + i + " = " + (num + i));
+
+	        }
+	    }
+
+	}
+
+
