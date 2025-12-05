f7d9a792243becf53289110ab6f47e2c1c8697fa
sriramsriram06 <ppriy37@gmail.com>
2025-12-05 11:21:16 +0530

    Add files via upload
    Learn Operator

---
+++ learnoperators.java
@@ -0,0 +1,18 @@
+package LearnAutomation1;
+
+public class learnoperators {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=20;
+		System.out.println("a+b:"+ (a+b));
+		System.out.println("a-b:"+ (a-b));
+		System.out.println("a*b:"+ (a+b));
+		System.out.println("a/b:"+ (a/b));
+		System.out.println("a%b:"+ (a%b));
+		
+		
+	
+	}
+
+}
