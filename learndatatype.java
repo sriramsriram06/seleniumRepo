ed7f2b7d7ae931d7d9397cd1ae5d5487b32ebc5f
sriramsriram06 <ppriy37@gmail.com>
2025-12-05 11:18:54 +0530

    Add files via upload
    learn datatypes

---
+++ learndatatype.java
@@ -0,0 +1,19 @@
+package LearnAutomation1;
+
+public class learndatatype {
+
+	public static void main(String[] args) {
+		
+	        int age = 20;
+	        char initial = 'S';
+	        String name = "Sriram";
+	        boolean isActive = true;
+
+	        System.out.println(age);
+	        System.out.println(initial);
+	        System.out.println(name);
+	        System.out.println(isActive);
+	   
+	}
+
+}
